d5246f2a1ce7e950d0c3654e5674f6c2ee260749
haishuokQAQ <haishuo_k@163.com>
2018-07-31 23:52:33 +0800

    Change a method of not oo solution

--- src/main/java/com/jschtest/SolutionNotOO.java
+++ src/main/java/com/jschtest/SolutionNotOO.java
@@ -8,23 +8,54 @@ public class SolutionNotOO {
 		while (pointer1 != null && pointer2 != null) {
 			//先检查是否为null
 			if (pointer1.next == null && pointer2.next != null) {
-				pointer1.next = new ListNode(0);
+				pointer1.next = pointer2.next;
 			}
-			//如果p1长于p2那么p2的值当作0处理
+			if (pointer1 == pointer2) {
+				if (added) {
+					pointer1.val ++;
+				}
+				if (pointer1.val >= 10) {
+					pointer1.val -= 10;
+					added = true;
+				}else {
+					added = false;
+				}
+				pointer1 = pointer1.next;
+				pointer2 = pointer2.next;
+				continue;
+			}
+			//如果p1长于p2那么p2的值当作0处理1
 			long p2 = pointer2 == null? 0: pointer2.val;
 			pointer1.val += p2;
 			//算进位
-			if (added) pointer1.val ++;
+			if (added) {
+				pointer1.val ++;
+			}
 			if (pointer1.val >= 10) {
 				pointer1.val -= 10;
 				added = true;
+			}else {
+				added = false;
 			}
 			pointer1 = pointer1.next;
 			if (pointer2 != null) pointer2 = pointer2.next;
 		}
 		return l1;
 	}
-    
+
+	
+	
+	public boolean solveAdded(ListNode l, boolean added) {
+		if (added) {
+			l.val ++;
+		}
+		if (l.val >= 10) {
+			l.val -= 10;
+			return true;
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		SolutionNotOO so = new SolutionNotOO();
 
